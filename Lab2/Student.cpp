dc3a1b30d12983b8203f3a55540ba79718d9876b
Igor Aheienko <agsertui@gmail.com>
2026-03-08 13:30:23 +0200

    Lab3: implemented copy/move constructors, static field, operator overloading and stream operators

--- Lab2/Student.cpp
+++ Lab2/Student.cpp
@@ -1,11 +1,24 @@
 #include "Student.h"
 #include <iostream>
 
+int Student::studentCount = 0;
 
 Student::Student() : Student("Unknown", 18, 1) {}
 
 Student::Student(std::string name, int age, int year)
-    : name(name), age(age), year(year) {}
+    : name(name), age(age), year(year) {
+    studentCount++;
+}
+
+Student::Student(const Student& other)
+    : name(other.name), age(other.age), year(other.year) {
+    std::cout << "Student copied\n";
+}
+
+Student::Student(Student&& other)
+    : name(std::move(other.name)), age(other.age), year(other.year) {
+    std::cout << "Student moved\n";
+}
 
 Student::~Student() {
     std::cout << "Student destroyed\n";
@@ -17,3 +30,24 @@ void Student::display() const {
               << ", Year: " << year << std::endl;
 }
 
+void Student::setAge(int age) {
+    this->age = age;   
+}
+
+int Student::getCount() {
+    return studentCount;
+}
+
+Student Student::operator+(const Student& other) {
+    return Student(name + "&" + other.name, age, year);
+}
+
+std::ostream& operator<<(std::ostream& os, const Student& s) {
+    os << s.name << " " << s.age << " " << s.year;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Student& s) {
+    is >> s.name >> s.age >> s.year;
+    return is;
+}
