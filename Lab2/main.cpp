5aa28602d6e8985b5d4c868210f03d9369f00a87
Igor Aheienko <agsertui@gmail.com>
2026-03-17 18:43:45 +0200

    Changed main.cpp

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -19,6 +19,13 @@ int main() {
     Student s5 = s1 + s2;
     std::cout << Student::getCount() << std::endl;
 
+    Teacher t1("Ivan", "Math");
+
+    Course c1("Algebra", "OldTeacher", 3);   // старий варіант
+    Course c2("Physics", &t1, 4);            // новий варіант
+
+    OnlineCourse c3("C++", &t1, 5, "Udemy");
+
     s1.display();
     s2.display();
 
